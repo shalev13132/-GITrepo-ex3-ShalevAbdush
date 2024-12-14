0460e10ada2050a80fec173ce458a65510502d47
shalev13132 <shalev13132@gmail.com>
2024-12-14 12:06:24 +0200

    just updated Shalev file

--- Shalev.java
+++ Shalev.java
@@ -1,7 +1,7 @@
 public class Shalev{
 	public static void main(String[] args){
 		for(int i=1;i<=5;i++){
-			System.out.println("Shalev Abdush");
+			System.out.println("The file Shalev has been changed ");
 		}
 	}
 }
